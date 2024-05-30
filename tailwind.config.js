09d450942daa8e19be7e13bcf5d95c8e7513ad25
Nipuna Theekshana <124187309+NipunaTheekshanaDeMel@users.noreply.github.com>
2024-05-31 04:32:15 +0530

    add files

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['*.{html,js}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+  daisyui: {
+    themes: ["sunset", "winter"],
+  },
+  safelist: [
+    'badge',
+    'badge-success',
+    'badge-warning',
+    'badge-error',
+    'badge-sm',
+    'alert',
+    'alert-success',
+    'alert-warning',
+    'alert-error',
+    'text-success',
+    'mask',
+    'mask-squircle'
+  ]
+}
+
