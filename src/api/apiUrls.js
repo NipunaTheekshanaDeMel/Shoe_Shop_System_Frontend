d19ded014bbed49a40b11e9f97db71c216fba057
Nipuna Theekshana <124187309+NipunaTheekshanaDeMel@users.noreply.github.com>
2024-05-31 04:58:55 +0530

    add api

---
+++ src/api/apiUrls.js
@@ -0,0 +1,16 @@
+const baseUrl = 'http://localhost:9090/shoesPVT/api/v1';
+
+
+export const CUSTOMER_URL = `${baseUrl}/customer`;
+
+export const EMPLOYEE_URL = `${baseUrl}/employee`;
+
+export const SUPPLIER_URL = `${baseUrl}/supplier`;
+
+export const INVENTORY_URL = `${baseUrl}/inventory`
+
+export const SALE_URL = `${baseUrl}/sale`
+
+export const AUTH_URL = `${baseUrl}/auth`
+
+export const PANEL_URL = `${baseUrl}/adminPanel`
